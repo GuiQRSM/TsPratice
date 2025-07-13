fb944a735d97dddaf889a3b27e9777213a685e9a
GuiQRSM <quaresmagui20@gmail.com>
2025-07-13 14:22:47 -0300

    add: liction string

---
+++ src/modulo-02/stringType.ts
@@ -0,0 +1,15 @@
+// ===> Single Quote
+
+import { createLanguageService } from "typescript";
+
+let nomeCompleto: string = 'João da Silva';
+console.log(nomeCompleto);
+
+// ===> Double Quote
+
+let funcaoEmpresa: String = "Desenvolvedor de Software";
+console.log(funcaoEmpresa);
+
+// ===> Back Ticks
+let nomeEmpresa: String = 'Empresa XYZ';
+console.log(`Bem vindo á ${nomeEmpresa} como ${funcaoEmpresa} do ${nomeCompleto}`);
