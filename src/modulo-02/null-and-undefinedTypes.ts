0ad70c0b439062ad8a1d89018f01bf804a035b1c
GuiQRSM <quaresmagui20@gmail.com>
2025-07-23 23:00:40 -0300

    rafactor(add): add null e undefined

---
+++ src/modulo-02/null-and-undefinedTypes.ts
@@ -0,0 +1,11 @@
+//; Ex 01: Null
+//const variaveltesteNull = null;
+
+//console.log(variaveltesteNull);
+//console.log(typeof variaveltesteNull);
+
+// Ex 02: Undefined
+let variavelTesteUndefined;
+
+console.log(variavelTesteUndefined);
+console.log(typeof variavelTesteUndefined);
