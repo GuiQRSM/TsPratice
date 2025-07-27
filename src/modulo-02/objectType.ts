cf478d0240147af6b660ff0ee5d8d319c803e7da
GuiQRSM <quaresmagui20@gmail.com>
2025-07-26 21:16:20 -0300

    finish the object settings

--- src/modulo-02/objectType.ts
+++ src/modulo-02/objectType.ts
@@ -53,4 +53,83 @@ function onboarding04(pessoa: Pessoa05) {
     return `Seja bem vindo ${pessoa.nome}, sua função é ${pessoa.funcao} e você trabalha com ${pessoa.linguagem}${pessoa.email ? `, seu email é ${pessoa.email}` : ''}`;
 }
 
-console.log(onboarding04({nome: 'Ana', funcao: 'Gerente', linguagem: 'JavaScript', email: 'bLr7o@example.com'}));
+//console.log(onboarding04({nome: 'Ana', funcao: 'Gerente', linguagem: 'JavaScript', email: 'bLr7o@example.com'}));
+
+// Ex 06 - Propiedade 'readonly' (se deseja proibir que os devs não modifiquem um deerminado objeto uso o 'readonly'))
+
+interface Pessoa06 {
+    nome: string;
+    funcao: string;
+    linguagem: string;
+    readonly email: string; // Propriedade opcional
+}
+
+function onboarding05(pessoa: Pessoa06) {
+    return `Seja bem vindo ${pessoa.nome}, sua função é ${pessoa.funcao} e você trabalha com ${pessoa.linguagem}${pessoa.email ? `, seu email é ${pessoa.email}` : ''}`;
+}
+
+//console.log(onboarding05({nome: 'Ana', funcao: 'Gerente', linguagem: 'JavaScript', email: 'bLr7o@example.com'}));
+
+// Ex 07 - Tipos de eztensões (heranças)
+interface Mae {
+    nome: string
+}
+interface Pai {
+    sobrenome: string
+}
+
+interface Filha extends Mae, Pai {
+idade: number;
+}
+
+const filha: Filha = {
+    nome: 'Ana',
+    sobrenome: 'Silva',
+    idade: 25,
+};
+
+//Ex 08 = Tipos de intersecções
+interface Cachoro {
+    tipo: string;
+}
+interface Gato {
+    tipo: string;
+}
+
+type Animal = Cachoro & Gato;
+
+// Ex 09 - Generic Objects
+type Usuario = {
+    nome: string;
+    email: string;
+}
+
+type Admin = {
+    nome: string;
+    email: string;
+    admin: boolean;
+}
+
+const usuario: Usuario = {
+    nome: 'Carlos',
+    email: 'BZTqI@example.com'
+}
+
+const admin: Admin = {
+    nome: 'Ana',
+    email: 'bLr7o@example.com',
+    admin: true
+}
+
+// Está função pode receber tanto um objeto do tipo Usuario quanto Admin
+function acessarSistema<T>(usuario: T): T {
+    return usuario;
+    
+}
+
+
+/*function acessarSistema(usuario: Usuario) {
+    return usuario;
+}*/
+
+//console.log(acessarSistema(usuario));   
